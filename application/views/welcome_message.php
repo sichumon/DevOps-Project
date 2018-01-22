257897f8c7d290aa0c076a9337dfed5f132dbe64
Tou Tong Vang <ttvang22@toutvanglaptop.Home>
2018-01-22 07:08:44 -0700

    Changing the titles

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,153 +1,51 @@
+
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title> Tou Tong Vang </title>
-    <link rel="stylesheet" href="styles/main.css"/>
-
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
-
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat"/>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Cardo"/>
-  </head>
-
-  <body>
-    <main class="overflow--auto">
-      <h1 class="color--skyBlue section__heading--largest">
-        <!--Your name goes here -->
-        Tou Tong Vang
-      </h1>
-
-      <ul class="section--social">
-
-        <!--Links to relevant professional social media & resume -->
-        <!-- See: http://fontawesome.io/icons/#brand for more -->
-
-        <!-- Link to Linked In profile -->
-        <li class="socialWrapper">
-          <a class="color--skyBlue social"
-             title="Facebook Profile"
-             target="_blank"
-             href="https://www.facebook.com/toutongvang22">
-            <i class="fa fa-facebook"></i>
-          </a>
-        </li>
-
-        <!-- Link to Linked In profile -->
-        <li class="socialWrapper">
-          <a class="color--skyBlue social"
-             title="LinkedIn Profile"
-             target="_blank"
-             href="https://www.linkedin.com/in/tou-vang-704428122/">
-            <i class="fa fa-linkedin"></i>
-          </a>
-        </li>
-
-        <!-- Link to GitHub profile -->
-        <li class="socialWrapper color--skyBlue">
-          <a class="social color--skyBlue"
-             title="GitHub Profile"
-             target="_blank"
-             href="https://github.com/ttvang22">
-            <i class="fa fa-github"></i>
-          </a>
-        </li>
-
-        <!-- Link to resume, probably a .pdf -->
-        <li class="socialWrapper">
-          <a class="social color--skyBlue"
-             title="Resume"
-             href="TouVangResume.pdf">
-            <i class="fa fa-file-text"></i>
-          </a>
-        </li>
-      </ul>
-    </main>
-
-    <section class="background--skyBlue section">
-      <h2 class="color--cloud margin--none section__text--centered">
-        Welcome To My ServerlessPortfolio
-      </h2>
-    </section>
-
-    <section class="section section--alignCentered section--description">
-      <div class="section__exampleWrapper">
-        <div class="section__example">
-          <img alt="example screenshot of a project involving code"
-               class="section__exampleImage"
-               src="images/Family.jpg"/>
-          <dl class="color--cloud">
-            <dt class="section__exampleTitle section__text--centered">
-              Family
-            </dt>
-            <dd></dd>
-          </dl>
-        </div>
-      </div>
-
-      <div class="section__exampleWrapper">
-        <div class="section__example">
-          <img alt="example screenshot of a project involving chemistry"
-               class="section__exampleImage"
-               src="images/Fishing.jpg"/>
-          <dl class="color--cloud">
-            <dt class="section__exampleTitle section__text--centered ">
-              Fishing
-            </dt>
-            <dd></dd>
-          </dl>
-        </div>
-      </div>
-
-      <div class="section__exampleWrapper">
-        <div class="section__example">
-          <img alt="example screenshot of a project involving cats"
-               class="section__exampleImage"
-               src="images/OfficeDesk.jpg"/>
-          <dl class="color--cloud">
-            <dt class="section__exampleTitle section__text--centered">
-              Work
-            </dt>
-            <dd></dd>
-          </dl>
-        </div>
-      </div>
-    </section>
-
-    <section class="background--skyBlue section">
-      <h2 class="color--cloud margin--none section__text--centered">
-        About Me
-      </h2>
-    </section>
-
-    <section class="section section--alignCentered section--description">
-      <p class="color--darkgrey section__description">
-        Hi Name Is Tou Tong Vang. This is my profolio that display my experience
-        in HTML, CSS, JAVA and Serverless Backend.
-      </p>
-
-    <div class="background--skyBlue modal--closed">
-      <span class="color--cloud modal__closeButton">
-        <i class="fa fa-window-close-o"></i>
-      </span>
-      <img alt="example screenshot of a project involving code"
-           class="modal__image"
-           src="images/example1.png"/>
-      <div class="color--cloud modal__text">
-        <h2 class="modal__title">
-          Work
-        </h2>
-        <a class="color--skyBlue modal__link"
-           href="#">
-          Check it out
-        </a>
-        <p class="modal__description">
-          A long description of the work in question.
-        </p>
-      </div>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Hello From Me</title>
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/jquery-ui.min.css">
+  <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/styles.css">
+  <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-ui.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url(); ?>js/jquery-ui.theme.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="<?php echo base_url(); ?>js/script.js"></script>
+</head>
+<body>
+<div id="header">
+  
+</div>
+<div class="container">
+  <div class="row">
+    <p>Please fill the following form to book a  ticket</p>
+  </div>
+  <div class="row">
+  <form action="/welcome/book" method="POST">
+    <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
     </div>
+    <div class="form-group">
+    <label for="passengers">Number of passengers</label>
+    <select class="form-control" name="passengers" id="passengers">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+    </select>
+    </div>
+    <div class="form-group">
+      <label for="departure">Departure date</label>
+      <input type="text" id="datepicker" name="departure">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+  </div>
+</div>
 
-  </body>
+</body>
 </html>
